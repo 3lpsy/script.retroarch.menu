714f8c008e0e5da857fa55ca1716c27a33a4379b
3lpsy <8757851+3lpsy@users.noreply.github.com>
2023-01-20 11:38:58 -0800

    Add more customization, logging

--- addon.py
+++ addon.py
@@ -1,7 +1,20 @@
 import xbmcaddon
 import xbmcvfs
 import subprocess
+
 addonPath = xbmcaddon.Addon('script.retroarch.menu').getAddonInfo('path')
-startCommand = xbmcaddon.Addon('script.retroarch.menu').getSettingString('start command')
+
+startRetroarchCommand = xbmcaddon.Addon('script.retroarch.menu').getSettingString('start-retroarch')
+stopKodiCommand = xbmcaddon.Addon('script.retroarch.menu').getSettingString('stop-kodi')
+restartKodiCommand = xbmcaddon.Addon('script.retroarch.menu').getSettingString('restart-kodi')
+
 logFile = xbmcvfs.translatePath('special://temp/retroarch.log')
-subprocess.Popen(['/bin/bash', addonPath + 'resources/bin/retroarch.sh', startCommand, logFile], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+subprocess.Popen([
+    '/bin/bash', 
+    addonPath + 'resources/bin/retroarch.sh', 
+    startRetroarchCommand, 
+    stopKodiCommand, 
+    restartKodiCommand, 
+    logFile
+], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
--- resources/bin/retroarch.sh
+++ resources/bin/retroarch.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-JSON_RPC_NOTIFICATION=$(curl -s --data-binary '{"jsonrpc": "2.0", "method": "GUI.ShowNotification", "params": {"title": "Please Wait", "message": "Running RetroArch Launch Script" },  "id":1}' -H 'content-type: application/json;' http://127.0.0.1:8080/jsonrpc)
+start_retroarch="$1"
+stop_kodi="$2"
+restart_kodi="$3"
+log_file="$4"
 
-if [ -f "$2" ]
-then
-    mv "$2" "$2.old"
+function log() {
+	echo "$@" >> $log_file;
+}
+
+if [ -f "$log_file" ]; then
+    mv "${log_file}" "${log_file}.old"
+	log "Replacing log file..."
 fi
 
-echo "JSONRPC response: $JSON_RPC_NOTIFICATION" > "$2"
+log "Stopping kodi with ${stop_kodi}";
+$stop_kodi >> "$log_file" 2>&1;
 
-sleep 1s
+log "Starting retroarch with ${start_retroarch}";
+$start_retroarch >> "$log_file" 2>&1
 
-JSON_RPC_NOTIFICATION=$(curl -s --data-binary '{"jsonrpc": "2.0", "method": "Application.Quit", "id":1}' -H 'content-type: application/json;' http://127.0.0.1:8080/jsonrpc)
-
-echo "JSONRPC response: $JSON_RPC_NOTIFICATION" >> "$2"
-
-# Wait for the quit
-sleep 5s
-
-retroarch --menu >> "$2" 2>&1
-
-echo "Restarting Kodi with $1" >> "$2"
-eval "$1"
+log "Restarting kodi with ${restart_kodi}";
+$restart_kodi
